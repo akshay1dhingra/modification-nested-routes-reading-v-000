2b1d489cb1d04650b4fe8cf476c4553634a9f3ce
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-21 17:38:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     if params[:author_id] 
       @author = Author.find_by(id: params[:author_id])
       if @author == nil 
-        
+        redirect_to authors_path, alert: "Author not found"
       else 
       
       end

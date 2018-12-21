8216cdc58b3a5c7d0236453f28b502f99136afa3
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-21 18:28:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
         redirect_to authors_path, alert: "Author not found"
       else 
         @post = @author.posts.find_by(id: params[:id])
-        redirect_to author_post
+        redirect_to author_posts_path(@author), 
       end
     else 
       @post = Post.find(params[:id])
